e51f9c0e1a6cd328a8470eb46392cc96b83c44f7
shravanimula <shravani.mula@thundersoft.com>
2023-08-22 18:23:47 +0530

    replacing the new word

---
+++ strings/str_replace.py
@@ -0,0 +1,8 @@
+s='let it be let it be'
+print(s.replace('let','yes'))
+
+s1=input("enter the input:")
+new=input("enter the new word:" )
+replace=input("enter replace word :")
+s2=s1.replace(replace,new)
+print(s2)
