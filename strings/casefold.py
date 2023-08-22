6719452033d21b938fad00107ff087a8cf251518
shravanimula <shravani.mula@thundersoft.com>
2023-08-22 18:27:30 +0530

    casefold and convert upper  and lower and swap case

---
+++ strings/casefold.py
@@ -0,0 +1,19 @@
+#casefold funtion
+s="AAA bbb CCCC vvvv"
+print(s.casefold())
+#converting string into upper
+s1=s.upper()
+print(s1)
+# convert string into lower
+s2=s.lower()
+print(s2)
+
+#converting after space capital letter
+s3=s2.title()
+print(s3)
+
+#converting upper to lower
+s4=s3.swapcase()
+print(s4)
+
+
