41a3b3b5d5e1782be15f1a0b9dce1d2774c8d28e
shravanimula <shravani.mula@thundersoft.com>
2023-08-23 18:09:59 +0530

    it will return string index

---
+++ strings/str_index.py
@@ -0,0 +1,12 @@
+st=input("enter a string:")
+sub=input("enter a substring:")
+re=st.index(sub)
+print(re)
+
+s="abcd efgh hi helo"
+substr="hi"
+r=s.index(substr)
+print(r)
+
+s1='hello hi hello abcd'
+print(s1.index('hi'))
