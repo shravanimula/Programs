2ec5e9604a73e0095b2dadf49b2931f6c4494954
shravanimula <shravani.mula@thundersoft.com>
2023-08-23 18:15:51 +0530

    string with leading characters stripped

---
+++ strings/str_strip.py
@@ -0,0 +1,12 @@
+s=input("enter a string:")
+print(s)
+print(s.lstrip())
+
+s1=input("enter a string1:")
+print(s1)
+print(s1.rstrip())
+
+s2=input("enter astring2:")
+print(s2)
+print(s2.strip())
+
