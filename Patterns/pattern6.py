3bc2b6b36e5d582b1240866bf94d0ac50fb8558b
shravanimula <98573934+shravanimula@users.noreply.github.com>
2023-09-28 18:15:16 +0530

    Patterns programs in python

---
+++ Patterns/pattern6.py
@@ -0,0 +1,12 @@
+'''
+5
+5 4
+5 4 3
+5 4 3 2
+5 4 3 2 1
+'''
+n=int(input("enter a number of rows:"))
+for i in range(n,0,-1):
+    for j in range(n,i-1,-1):
+        print(j,end=' ')
+    print(' ')
